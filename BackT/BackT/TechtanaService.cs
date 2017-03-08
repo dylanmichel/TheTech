4dad7cdfb7ee2cb5600e88dac8f102bf8a1c71c0
Dylan Michel <502707467@ge.com>
2017-03-08 11:09:24 -0500

    added task

--- BackT/BackT/TechtanaService.cs
+++ BackT/BackT/TechtanaService.cs
@@ -10,8 +10,16 @@ namespace BackT.VoiceCommands
     public sealed class TechtanaService : IBackgroundTask
     {
         
-        
+
+        public void Run(IBackgroundTaskInstance taskInstance)
         {
+            var builder = new BackgroundTaskBuilder();
+            builder.Name = "My Background Trigger";
+            builder.SetTrigger(new TimeTrigger(15, true));
+            // Do not set builder.TaskEntryPoint for in-process background tasks
+            // Here we register the task and work will start based on the time trigger.
+            BackgroundTaskRegistration task = builder.Register();
+
 
         }
 
